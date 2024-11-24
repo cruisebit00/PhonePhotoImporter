58804215184b833f41e62e0b89da6762452b8627
Alex <alex_carmeli@hotmail.com>
2024-11-24 11:36:26 -0500

    Copy/overwrite photo to destination directory.

--- PhonePhotoImporter.py
+++ PhonePhotoImporter.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 
 # Importer parameters
 source_dir = "TestSrc"
@@ -41,32 +42,48 @@ def GetFileDate(a_file):
         return ""
     return file_date_str[:4] + "-" + file_date_str[4:6] + "-" + file_date_str[6:]
 
-ClearDir(dest_dir)
+#ClearDir(dest_dir)
 all_files = GetSourceFiles(source_dir)
 for src_file_name in all_files:
-    src_file_path = os.path.join(source_dir, src_file_name)
+    src_file_full_name = os.path.join(source_dir, src_file_name)
 
     #
     # The file must pass various checks to be imported.
     #
-    file_qualification = QualifyFile(src_file_path)
+    file_qualification = QualifyFile(src_file_full_name)
     if not file_qualification["qualified"]:
-        print(src_file_path, file_qualification["reason"])
+        print(src_file_name, file_qualification["reason"])
         continue
 
     #
     # Get the file's date. It will be used to name the file's destination folder.
     #
-    file_date = GetFileDate(src_file_path)
+    file_date = GetFileDate(src_file_full_name)
     if file_date == "":
-        print(src_file_path, "Error parsing file name")
+        print(src_file_name, "Error parsing file name")
         continue
 
+    # If the photo's folder does not exist, create it.
     dest_file_path = os.path.join(dest_dir, file_date)
     if not os.path.isdir(dest_file_path):
         try:
             os.mkdir(dest_file_path)
         except OSError:
-            print(src_file_path, "Error creating directory " + dest_file_path)
+            print(src_file_name, "Error creating directory " + dest_file_path)
             continue
-    print("OK:", src_file_path, "-", file_date)
+    
+    #
+    # Copy the file to its destination folder.
+    #
+    dest_file_full_name = os.path.join(dest_file_path, src_file_name)
+    action_string = "copied"
+    if os.path.isfile(dest_file_full_name):
+        action_string = "overwritten"
+    
+    try:
+        shutil.copy2(src_file_full_name, dest_file_full_name)
+    except Exception as e:
+        print(src_file_name, "Error copying file to", dest_file_path, ": " + e.strerror)
+        continue
+
+    print("OK:", src_file_name, action_string)
