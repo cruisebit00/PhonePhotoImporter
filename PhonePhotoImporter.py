122d589b56c83fc90eb3bad8c490ec8df1caa6b9
Alex <alex_carmeli@hotmail.com>
2025-12-08 18:46:33 -0500

    Command line arguments

--- PhonePhotoImporter.py
+++ PhonePhotoImporter.py
@@ -1,32 +1,36 @@
 import os
 import shutil
+import sys
+
+# Supporting libraries
+from PIL import Image
 
 # Importer parameters
 from defaults import *
 from utils import *
 
 
-def ClearDir(a_dir):
-    if a_dir != dest_dir:
-        print("Not clearing non-test dir", a_dir)
-        return
-    try:
-        files = os.listdir(a_dir)
-        for file in files:
-            file_path = os.path.join(a_dir, file)
-            if os.path.isfile(file_path):
-                os.remove(file_path)
-        print("Destination dir", a_dir, "cleared.")
-    except OSError:
-        print("Error occured clearing dir", a_dir)
+# def ClearDir(a_dir):
+#     if a_dir != dest_dir:
+#         print("Not clearing non-test dir", a_dir)
+#         return
+#     try:
+#         files = os.listdir(a_dir)
+#         for file in files:
+#             file_path = os.path.join(a_dir, file)
+#             if os.path.isfile(file_path):
+#                 os.remove(file_path)
+#         print("Destination dir", a_dir, "cleared.")
+#     except OSError:
+#         print("Error occured clearing dir", a_dir)
 
 
-def QualifyFile(a_file):
-
+def QualifyFileSize(a_file):
     # File too small
-    size_threshold = 600*1024
+    # Default threshold is 100KB. Genuine files as small as 150KB have been found.
+    size_threshold = 100*1024
     if os.path.getsize(a_file) < size_threshold:
-         return {"qualified": False, "reason": "Size under threshold " + f"{size_threshold:,}"}
+         return {"qualified": False, "reason": "Size under threshold of " + f"{size_threshold:,}"}
     
     return {"qualified": True, "reason": ""}
 
@@ -70,7 +74,7 @@ def ImportPhonePhotos(a_source_dir, a_dest_dir):
         #
         # The file must pass various checks to be imported.
         #
-        file_qualification = QualifyFile(src_file_full_name)
+        file_qualification = QualifyFileSize(src_file_full_name)
         if not file_qualification["qualified"]:
             print(src_file_name, file_qualification["reason"])
             continue
@@ -112,10 +116,33 @@ def ImportPhonePhotos(a_source_dir, a_dest_dir):
 
         print("OK:", src_file_name, action_string)
 
+def DisplayHelp():
+    help_text = """
+Options:
+  -h, /h     Show this help message
+  -t, /t     Display "test"
+  """
+    print(help_text)
+    
 def main():
-    source_dir = default_source_dir
-    dest_dir = default_dest_dir
-    ImportPhonePhotos(source_dir, dest_dir)
+    args = sys.argv[1:] # Skip script name
+
+    if not args:
+        print("No arguments passed.")
+        DisplayHelp()
+        return
+    
+    for arg in args:
+        if arg in ("-h", "/h"):
+            DisplayHelp()
+            return
+        elif arg in ("-t", "/t"):
+            source_dir = default_source_dir
+            dest_dir = default_dest_dir
+            print(">>> Testing mode ")
+            print(f"      From: {source_dir}")
+            print(f"        To: {dest_dir}")
+            ImportPhonePhotos(source_dir, dest_dir)
 
 if __name__ == "__main__":
     main()
